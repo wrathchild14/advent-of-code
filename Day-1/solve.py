9cc4212cf0a122589afbdbe40a5aa76e23f48caf
wrath <jovanprodanov123@gmail.com>
2021-12-10 20:25:10 +0100

    Day 1 Done

--- Day-1/solve.py
+++ Day-1/solve.py
@@ -1,14 +1,34 @@
 with open('input') as f:
     lines = f.readlines()
 
-prev = 0
-count = 0
-for line in lines:
-    now = int(line)
-    if (now > prev):
-        count += 1
-        print(now, "(increased)")
-    print(now, "(decreased)")
-    prev = now
-    
-print("Count:", count - 1) # subtract one because of the first measurment
+def first(list):
+    prev = 0
+    count = 0
+    for line in list:
+        now = int(line)
+        if (now > prev):
+            count += 1
+            # print(now, "(increased)")
+        # print(now, "(decreased)")
+        prev = now
+    return count - 1 # subtract one because of the first measurment
+
+def second(list):
+    prevSum = 0
+    count = 0
+    for i in range(1, len(list)):
+        if (i + 1 < len(list)):
+            currSum = int(list[i-1]) + int(list[i]) + int(list[i+1])
+        else:
+            break
+        
+        if (currSum > prevSum):
+            count += 1
+            # print(currSum, "(increased)")
+        # print(currSum, "(decreased)")
+        prevSum = currSum
+        
+    return count - 1
+        
+# print("Count:", first(lines)) 
+print("Count:", second(lines))
